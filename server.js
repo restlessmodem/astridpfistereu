71adc6b01c50f010537fdd2fab6d2bf467f39a92
Christopher Pfister <christopherpfisterde@protonmail.com>
2020-01-21 21:26:16 +0100

    Updated server redirection settings, impressum

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const compression = require('compression')
 const data = require('./data.json');
 const app = express();
 const server = app.listen(8080, () => {
     console.log(`Server running on port 8080`);
 });
 
+app.disable('x-powered-by'); // Disable express x-powered-by header
+
+// Compress all responses if no x-no-compression header set
+app.use(compression({ filter: function (req, res) {
+    if (req.headers['x-no-compression']) {
+      return false
+    }
+    // fallback to standard filter function
+    return compression.filter(req, res)
+  } 
+}))
+ 
+// If request is not https or contains www then reroute
+app.use(function(req,resp,next){
+    if (req.headers['x-forwarded-proto'] == 'http' || req.headers.host.includes("www.")) {
+        return resp.redirect(301, 'https://' + req.headers.host.replace("www.", "") + '/');
+    } else {
+        return next();
+    }
+});
+
+// render pug view on root
 app.get('/', (req, res) => {
     res.render('index', {
         data: data.category
